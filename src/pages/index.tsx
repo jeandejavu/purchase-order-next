1d1135aca625634e30784bf434b6d81e92c330da
Jean Melo <jeandejavu@hotmail.com>
2021-02-02 11:59:36 -0300

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Home: React.FC = () => {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  purge: ['./src/pages/**/*.ts', './src/components/**/*.ts'],
+  darkMode: 'class', // false or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#faf5ff',
+          100: '#f3e8ff',
+          200: '#e9d5ff',
+          300: '#d8b4fe',
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+          700: '#7e22ce',
+          800: '#6b21a8',
+          900: '#581c87',
+        },
+        secondary: {
+          50: '#fdf2f8',
+          100: '#fce7f3',
+          200: '#fbcfe8',
+          300: '#f9a8d4',
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d',
+          900: '#831843',
+        },
+      },
+      fontFamily: {
+        display: ['Roboto Mono', 'Menlo', 'monospace'],
+        body: ['Roboto Mono', 'Menlo', 'monospace'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
