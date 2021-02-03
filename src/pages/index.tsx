bf8b4f6526e6fac40976befb10328ce1227bd2c9
Jean Melo <jeandejavu@hotmail.com>
2021-02-03 20:19:52 -0300

    fix: correcao botao esc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,7 @@
-import { useState } from 'react'
-import CadastroProduto from '../components/CadastroProduto'
 import Container from '../components/Container'
-import Modal from '../components/Modal'
 
 const Home: React.FC = () => {
-  const [isModalVisible, setModalVisible] = useState(true)
-  return (
-    <Container>
-      {isModalVisible ? (
-        <Modal
-          sizeMedium={false}
-          onClose={() => {
-            setModalVisible(false)
-          }}
-        >
-          <CadastroProduto />
-        </Modal>
-      ) : (
-        <></>
-      )}
-    </Container>
-  )
+  return <Container />
 }
 
 export default Home
